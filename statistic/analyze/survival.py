3e62fab50d243798dd5077ed600e3fc90de322a6
Lin Yiming <linym@jadenine.com>
2016-02-02 11:35:07 +0800

    ADD: analyze survival rate

--- statistic/analyze/survival.py
+++ statistic/analyze/survival.py
@@ -2,10 +2,13 @@
 # coding:utf-8
 import logging
 
-from dateutil.relativedelta import relativedelta
+from datetime import timedelta
+
+from django.utils import timezone
+from monthdelta import MonthDelta
 
 from statistic.models import UserSurvival
-from statistic.util.datehelper import trim_2_local_day_start, week_start, month_start
+from statistic.util.datehelper import week_start, month_start, day_start
 
 logger = logging.getLogger(__name__)
 
@@ -49,27 +52,26 @@ def cal_x_length(start_date, end_date, group_window):
 
 
 def get_index(start_date, group_window, time):
-    result = 0
+    index = 0
     if 1 == group_window:
         delta = time - start_date
-        result = delta.days
+        index = delta.days
     if 2 == group_window:
         start_date = week_start(start_date)
         time = week_start(time)
 
         delta = time - start_date
-        result = delta.days / 7
+        index = delta.days / 7
     elif 3 == group_window:
         start_date = month_start(start_date)
         time = month_start(time)
 
-        result = (time.year - start_date.year) * 12 + (time.month - start_date.month)
+        index = (time.year - start_date.year) * 12 + (time.month - start_date.month)
 
-    return result, time
+    return index, time
 
 
 def is_survival(user_survival, survival_interval):
-
     survival = False
     if 1 == survival_interval:
         survival = user_survival.survival_day()
@@ -79,3 +81,41 @@ def is_survival(user_survival, survival_interval):
         survival = user_survival.survival_month()
 
     return survival
+
+
+def get_survival_rate(anchor_date, group_window=2, unit=2):
+    start_time, end_time = window_range(anchor_date, group_window)
+
+    user_survivals = UserSurvival.objects.filter(firsttime__range=[start_time, end_time])
+    user_count = len(user_survivals)
+
+    x_count = cal_x_length(start_time, timezone.now(), unit)
+
+    values = [{'x': i, 'y': 0} for i in range(x_count)]
+    for user_survival in user_survivals:
+        index, _ = get_index(anchor_date, unit, user_survival.lasttime)
+        values[index]['y'] += 1
+
+    for index in range(x_count - 2, -1, -1):
+        values[index]['y'] += values[index + 1]['y']
+
+    return {
+        'start_date': start_time,
+        'user_window': group_window,
+        'unit': unit,
+        'values': values,
+        'total': user_count
+    }
+
+
+def window_range(anchor_date, group_window):
+    if 1 == group_window:
+        start_time = day_start(anchor_date)
+    elif 2 == group_window:
+        start_time = week_start(anchor_date)
+        end_time = start_time + timedelta(days=7)
+    elif 3 == group_window:
+        start_time = month_start(anchor_date)
+        end_time = start_time + MonthDelta(months=1)
+
+    return start_time, end_time
--- statistic/analyze/tests.py
+++ statistic/analyze/tests.py
@@ -6,8 +6,8 @@ from dateutil.relativedelta import relativedelta
 from django.test import TestCase
 from django.utils import timezone
 
-from statistic.analyze.survival import cal_x_length, get_survival_rate_tendency
-from statistic.util.datehelper import month_start
+from statistic.analyze.survival import cal_x_length, get_survival_rate_tendency, get_survival_rate
+from statistic.util.datehelper import month_start, week_start
 from statistic.models import UserSurvival
 
 logger = logging.getLogger(__name__)
@@ -48,3 +48,34 @@ class Test(TestCase):
         self.assertIsNone(result[3])
         self.assertIsNone(result[4])
 
+
+class TestSurvivalRate(TestCase):
+    def setUp(self):
+        self.day1 = week_start(timezone.now() - timedelta(days=60))
+
+        self.day2 = self.day1 + timedelta(days=1)
+        self.day_next_week_1 = self.day1 + timedelta(days=7)
+        self.day_next_week_2 = self.day2 + timedelta(days=7)
+        self.day_next_month_1 = self.day1 + relativedelta(months=1)
+        self.day_next_month_2 = self.day2 + relativedelta(months=1)
+
+        UserSurvival.objects.create(imei='imei1', firsttime=self.day1, lasttime=self.day1)
+        UserSurvival.objects.create(imei='imei2', firsttime=self.day1, lasttime=self.day2)
+        UserSurvival.objects.create(imei='imei3', firsttime=self.day2, lasttime=self.day_next_month_2)
+        UserSurvival.objects.create(imei='imei4', firsttime=self.day2, lasttime=self.day_next_week_2)
+
+    def test_get_survival_rate(self):
+        result = get_survival_rate(self.day1, 2, 2)
+        self.assertEqual(2, result['unit'])
+        self.assertEqual(2, result['user_window'])
+        self.assertEqual(week_start(self.day1), result['start_date'])
+        self.assertEqual(4, result['total'])
+
+        values = result['values']
+        self.assertEqual(4, values[0]['y'])
+        self.assertEqual(2, values[1]['y'])
+        self.assertEqual(1, values[2]['y'])
+        self.assertEqual(1, values[3]['y'])
+        self.assertEqual(1, values[4]['y'])
+        for x in range(5, 10):
+            self.assertEqual(0, values[x]['y'])
